cbd1fc35a305030d6472ea3f21a707e04555611f
Marvin Gay <MarvinJGay@protonmail.com>
2020-06-30 19:42:10 -0400

    Added User Schema

---
+++ src/models/user.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+const uniqueValidator = require('mongoose-unique-validator');
+
+const { Schema, model } = mongoose;
+
+const userSchema = new Schema({
+  authID: { type: String, required: true, unique: true },
+  displayName: { type: String, required: true, unique: true, minlength: 3 },
+  role: { type: String, required: true },
+});
+
+userSchema.plugin(uniqueValidator);
+
+const User = model('User', userSchema);
+
+module.exports = User;
