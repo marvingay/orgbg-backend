5eac532b8e6a3a636eef27849700937831006967
Marvin Gay <MarvinJGay@protonmail.com>
2020-08-12 12:58:29 -0400

    Fixed: Delete Route Async

--- src/routes/users.js
+++ src/routes/users.js
@@ -2,9 +2,9 @@ const express = require('express');
 const router = express.Router();
 const User = require('../models/user');
 
-router.delete('/:displayName', (req, res) => {
+router.delete('/:displayName', async (req, res) => {
   await User.findOneAndDelete({ displayName: req.params.displayName });
-  return res.status(204).json({ msg: "Account Deleted" });
-})
+  return res.status(204).json({ msg: 'Account Deleted' });
+});
 
 module.exports = router;
